5fa8e13d0b9fc116d3dc4aca9463c8b4c8ab49ec
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2016-08-13 17:23:56 +0200

    Added setup.cmd to simplify running setup module
    Signed-off-by: Hajime Yamasaki Vukelic <hayavuk@gmail.com>

---
+++ setup.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+REM
+REM Seagull photo gallery app
+REM Copyright (C) 2016  Hajime Yamasaki Vukelic
+REM
+REM This program is free software: you can redistribute it and/or modify it
+REM under the terms of the GNU General Public License as published by the Free
+REM Software Foundation, either version 3 of the License, or (at your option)
+REM any later version.
+REM
+REM This program is distributed in the hope that it will be useful, but WITHOUT
+REM ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+REM FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+REM more details.
+REM
+
+python setup.py %*
